bd81d94804dd8f4d502b8606302d2717e262baf3
dmiller <daxdoxsi@gmail.com>
2017-07-18 18:24:42 -0500

    Reading remote files

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php
 
-phpinfo();
+if (!isset($_GET["site"])):?>
+
+<form method="GET" action="">
+<input type="text" name="site" />
+<input type="submit" value="GET" />
+</form>
+
+<?php
+else:
+$content = file_get_contents($_GET["site"]);
+
+?>
+
+<?php echo $content; ?>
+
+<?php
+endif;
+?>
 
